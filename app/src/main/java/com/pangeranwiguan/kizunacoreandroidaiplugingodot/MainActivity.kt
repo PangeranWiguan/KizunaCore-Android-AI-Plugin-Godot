499a2b1285dc05e75ac9def59f9c33b74215cacf
PangeranWiguan <pangeranwiguan@gmail.com>
2025-02-28 20:58:09 +0800

    Include llama.cpp folder, test build the project and got the .so file.
    The .so file is not yet usable, just testing the inclusion of the llama.cpp only at this stage,  because too many errors before.

---
+++ app/src/main/java/com/pangeranwiguan/kizunacoreandroidaiplugingodot/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.pangeranwiguan.kizunacoreandroidaiplugingodot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pangeranwiguan.kizunacoreandroidaiplugingodot.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'kizunacoreandroidaiplugingodot' native library,
+     * which is packaged with this application.
+     */
+    private external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'kizunacoreandroidaiplugingodot' library on application startup.
+        init {
+            System.loadLibrary("kizunacoreandroidaiplugingodot")
+        }
+    }
+}
